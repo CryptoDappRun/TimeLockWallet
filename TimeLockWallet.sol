31a8128d3a85a72e5d5cd5d70e6be97ec76b8c3d
CryptoDappRun <122711721+CryptoDappRun@users.noreply.github.com>
2024-09-04 09:49:41 +0800

    Update TimeLockWallet.sol

--- TimeLockWallet.sol
+++ TimeLockWallet.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 //send token to checksum contract address directly
 //send eth directly
 
-
 interface IERC20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
@@ -15,7 +14,7 @@ interface IERC20 {
 contract TimeLockedWallet {
     address public owner;
     uint256 public lastWithdrawDate;
-    uint256 public constant LOCK_PERIOD = 5 minutes;
+    uint256 public LOCK_PERIOD; // Period in seconds
     uint256 public immutable WITHDRAWETH_WEI_AMOUNT;
     uint256 public immutable WITHDRAWTOKEN_AMOUNT;
 
@@ -34,9 +33,11 @@ contract TimeLockedWallet {
     event DepositedToken(address indexed token, address indexed sender, uint256 amount);
     event WithdrawnToken(address indexed token, address indexed owner, uint256 amount);
 
-    constructor(uint256 _withdrawETHWeiAmount, uint256 _withdrawTokenAmount) {
+    constructor(uint256 _lockPeriodDays, uint256 _withdrawETHWeiAmount, uint256 _withdrawTokenAmount) {
         owner = msg.sender;
         lastWithdrawDate = block.timestamp;
+        //LOCK_PERIOD = _lockPeriodDays * 1 minutes; // minutes for test
+        LOCK_PERIOD = _lockPeriodDays * 1 days; // Convert days to seconds
         WITHDRAWETH_WEI_AMOUNT = _withdrawETHWeiAmount;
         WITHDRAWTOKEN_AMOUNT = _withdrawTokenAmount;
     }
@@ -45,21 +46,18 @@ contract TimeLockedWallet {
         emit DepositedETH(msg.sender, msg.value);
     }
 
-// useless. you can send token to  contract chesum address directly.
-    /*
-
     function depositToken(address token, uint256 amount) external {
         IERC20 tokenContract = IERC20(token);
+        uint8 tokenDecimals = tokenContract.decimals();
+        uint256 amountAdjusted = amount * (10 ** tokenDecimals);
+        
         uint256 allowance = tokenContract.allowance(msg.sender, address(this));
-        require(allowance >= amount, "Check the token allowance");
+        require(allowance >= amountAdjusted, "Check the token allowance");
         
-        require(tokenContract.transferFrom(msg.sender, address(this), amount), "Token transfer failed");
+        require(tokenContract.transferFrom(msg.sender, address(this), amountAdjusted), "Token transfer failed");
         
-        emit DepositedToken(token, msg.sender, amount);
+        emit DepositedToken(token, msg.sender, amountAdjusted);
     }
-
-
-    */
 
 
     function withdrawETH() external onlyOwner canWithdraw {
@@ -80,11 +78,7 @@ contract TimeLockedWallet {
         emit WithdrawnToken(token, owner, withdrawAmountAdjusted);
     }
 
-    function getETHBalance() external view returns (uint256) {
-        return address(this).balance;
-    }
 
-    function getTokenBalance(address token) external view returns (uint256) {
-        return IERC20(token).balanceOf(address(this));
-    }
 }
+
+
